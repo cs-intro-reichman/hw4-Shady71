f045b2ba439fa8731e72badbd7be35e4e243e03d
Shady Gara <shadygara@h-MacBook-Pro-sl-Shady.local>
2024-12-03 21:14:57 +0200

    Find # of primes in a given range

--- Primes.java
+++ Primes.java
@@ -1,5 +1,33 @@
+import java.net.Socket;
+
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
-    }
+        int n = Integer.parseInt(args[0]);
+        boolean[] arr = new boolean[n + 1];
+        int counter = 0;
+
+        System.out.println("Prime numbers up to " + n + ":");
+        
+        for (int i = 2; i < n + 1; i++) {
+            if (i != 2 && i % 2 == 0) {
+                arr[i] = false;
+            }
+            else if (i != 3 && i % 3 == 0) {
+                arr[i] = false;
+            }
+            else if (i != 5 && i % 5 == 0) {
+                arr[i] = false;
+            }
+            else {
+                arr[i] = true;
+            }
+            if (arr[i] == true) {
+                System.out.println(i);
+                counter++;
+            }
+        }
+
+        int percentage = (int) ((double) counter / n * 100);
+        System.out.println("There are " + counter + " primes between 2 and " + n + " (" + percentage + "% are primes)" );
+    } 
 }
