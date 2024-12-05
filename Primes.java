57d871d59651f47cccc34e4f1ca64af634693b06
Shady Gara <shadygara@h-MacBook-Pro-sl-Shady.local>
2024-12-05 17:49:23 +0200

    Finds # of primes given a num

--- Primes.java
+++ Primes.java
@@ -1,32 +1,31 @@
-import java.net.Socket;
 
 public class Primes {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         boolean[] arr = new boolean[n + 1];
         int counter = 0;
-
+        arr[0] = false;
+        arr[1] = false;
         System.out.println("Prime numbers up to " + n + ":");
         
         for (int i = 2; i < n + 1; i++) {
-            if (i != 2 && i % 2 == 0) {
-                arr[i] = false;
-            }
-            else if (i != 3 && i % 3 == 0) {
-                arr[i] = false;
-            }
-            else if (i != 5 && i % 5 == 0) {
-                arr[i] = false;
-            }
-            else {
                 arr[i] = true;
             }
-            if (arr[i] == true) {
+        for (int i = 2; i < n + 1; i++) {
+            if (arr[i]) {
+                int j = i * i;
+                while (j < n + 1) {
+                    arr[j] = false;
+                    j += i;
+                }            
+            }
+        }
+        for (int i = 2; i < n + 1; i++) {
+            if (arr[i]) {
                 System.out.println(i);
                 counter++;
             }
         }
-
         int percentage = (int) ((double) counter / n * 100);
         System.out.println("There are " + counter + " primes between 2 and " + n + " (" + percentage + "% are primes)" );
     } 
